ab27ce98dfcb509748d7f5db85f1d1c94dd5a219
Geoff Shannon <geoffpshannon@gmail.com>
2019-01-24 00:52:34 -0800

    If there's no pads, I'm not interested in that module

--- assign_blocks.py
+++ assign_blocks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2.7
 
+# board = pcbnew.LoadBoard("/home/geoff/work/provel/printer/board/printer-board.kicad_pcb")
+
 def info(object, spacing=10, collapse=1):
     """Print methods and doc strings.
     Takes module, class, list, dictionary, or string."""
@@ -16,6 +18,12 @@ def main():
     board = GetBoard()
 
     modules = board.GetModules() # Iterable, not indexable
+    for module in modules:
+        pads = list(module.Pads())
+        if len(pads) > 0:
+            for pad in pads:
+                net = pad.GetNet()
+
 
 
 if __name__ == "__main__":

070157ffa5d58e4843e8df7ebf65125306862b75
Geoff Shannon <geoffpshannon@gmail.com>
2019-01-24 03:10:30 -0800

    Convolute the module data into a dict of netcode -> net info

--- assign_blocks.py
+++ assign_blocks.py
@@ -2,21 +2,46 @@
 
 # board = pcbnew.LoadBoard("/home/geoff/work/provel/printer/board/printer-board.kicad_pcb")
 
+from collections import defaultdict
 from pcbnew import GetBoard
 
 
 def get_pad_info(pad):
-    return (pad.GetName(), {'netcode': pad.GetNetCode(), 'netname': pad.GetNetname()})
+    pinname = str(pad.GetName())
+    return (pinname,
+            {'pinname': pinname,
+             'netcode': pad.GetNetCode(),
+             'netname': pad.GetNetname()})
 
 
 def get_module_info(module):
     pads = {padname: pad for padname, pad in map(get_pad_info, module.Pads())}
-    return (module.GetReference(), {'description': module.GetDescription(), 'pads': pads})
+    refdes = module.GetReference()
+    return (refdes,
+            {'refdes': refdes,
+             'description': module.GetDescription(),
+             'pads': pads})
+
+
+def get_all_module_info(board):
+    return {refdes: module for refdes, module in
+            map(get_module_info, board.GetModules())}
+
+
+def make_netlist(modules):
+    nets = defaultdict(lambda: {'pads': []})
+    for refdes, module in modules.items():
+        for pin_name, pin in module['pads'].items():
+            net = nets[pin['netcode']]
+            net['netname'] = pin['netname']
+            net['pads'].append((refdes, pin_name))
+
+    return nets
 
 
 def main():
-    board = GetBoard()
-    modules = {refdes: module for refdes, module in map(get_module_info, board.GetModules())}
+    modules = get_all_module_info(GetBoard())
+    netlist = make_netlist(modules)
 
 
 if __name__ == "__main__":
